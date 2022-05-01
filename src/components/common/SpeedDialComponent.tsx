eb0dde612acaef321202dfc61009cc060afa3df5
Juan Ibarra <jmibarra86@gmail.com>
2022-05-01 15:09:51 -0300

    Comienzo a modificar el boton para agregar elementos

--- src/components/common/SpeedDialComponent.tsx
+++ src/components/common/SpeedDialComponent.tsx
@@ -2,16 +2,12 @@ import Box from '@mui/material/Box';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
+import { AddTask, DirectionsRun, NotesOutlined } from '@mui/icons-material';
 
 const actions = [
-  { icon: <FileCopyIcon />, name: 'Copy' },
-  { icon: <SaveIcon />, name: 'Save' },
-  { icon: <PrintIcon />, name: 'Print' },
-  { icon: <ShareIcon />, name: 'Share' },
+  { icon: <AddTask />, name: 'Tarea' },
+  { icon: <NotesOutlined />, name: 'Nota' },
+  { icon: <DirectionsRun />, name: 'Hábito' },
 ];
 
 export default function SpeedDialComponent() {
