5ab427522ec3c081ac9bd738b411fb5980da9eec
Juan Ibarra <jmibarra86@gmail.com>
2022-06-18 13:20:26 -0300

    Se sacan los estilos de los componentes de notas a una clase usando styled components

--- src/components/notes/Notes.tsx
+++ src/components/notes/Notes.tsx
@@ -1,34 +1,13 @@
-import React from 'react'
-import { styled } from '@mui/material/styles';
-import { Paper } from "@mui/material";
-import Note from './Note';
 import NoteList from './NoteList';
+import { ItemHeader,Item } from './styles/NotesStyles';
 
 const Notes = () => {
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-        marginTop: '20px'
-    }));
-
-    const ItemHeader = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-        marginTop: '50px'
-    }));
 
     return (
         <>   
             <ItemHeader><h1>Notes</h1></ItemHeader>
             <Item><NoteList/></Item>
-        </>
-        
+        </> 
     )
 }
 
