5da5c79ca128622b18f31bcea8f00e9d6019635e
Juan Ibarra <jmibarra86@gmail.com>
2024-12-28 12:45:07 -0300

    se mejora el componente prioridades para poder mediante un estado ir cambiando dinámicamente la prioridad

--- src/components/common/Priority/Priority.tsx
+++ src/components/common/Priority/Priority.tsx
@@ -1,31 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+	IconButton,
+	Menu,
+	MenuItem,
+	ListItemIcon,
+	ListItemText,
+} from "@mui/material";
 import PriorityHighIcon from "@mui/icons-material/PriorityHigh";
 import { blue, grey, red, yellow } from "@mui/material/colors";
 import LowPriorityIcon from "@mui/icons-material/LowPriority";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
-import { IconButton } from "@mui/material";
 
 interface Props {
-	priority: Number | undefined;
+	priority: number | undefined;
 	taskId: string;
 	updatePriority: (id: string, newPriority: number) => void;
 }
 
-const Priority = ({ priority, taskId }: Props) => {
+const Priority = ({ priority, taskId, updatePriority }: Props) => {
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const [currentPriority, setCurrentPriority] = useState<number | undefined>(
+		priority
+	);
+
+	const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	const handleSelectPriority = (newPriority: number) => {
+		setCurrentPriority(newPriority);
+		updatePriority(taskId, newPriority);
+		handleClose();
+	};
+
 	return (
-		<IconButton
-			edge="end"
-			aria-label="delete-action"
-			onClick={() => {
-				alert(taskId);
-			}}
-		>
-			{!priority && <QuestionMarkIcon sx={{ color: grey[500] }} />}
-			{priority === 1 && <PriorityHighIcon sx={{ color: red[500] }} />}
-			{priority === 2 && <LowPriorityIcon sx={{ color: yellow[500] }} />}
-			{priority === 3 && <AcUnitIcon sx={{ color: blue[500] }} />}
-		</IconButton>
+		<>
+			<IconButton edge="end" onClick={handleOpen}>
+				{!currentPriority && (
+					<QuestionMarkIcon sx={{ color: grey[500] }} />
+				)}
+				{currentPriority === 1 && (
+					<PriorityHighIcon sx={{ color: red[500] }} />
+				)}
+				{currentPriority === 2 && (
+					<LowPriorityIcon sx={{ color: yellow[500] }} />
+				)}
+				{currentPriority === 3 && (
+					<AcUnitIcon sx={{ color: blue[500] }} />
+				)}
+			</IconButton>
+
+			<Menu
+				anchorEl={anchorEl}
+				open={Boolean(anchorEl)}
+				onClose={handleClose}
+				MenuListProps={{
+					"aria-labelledby": "priority-menu",
+				}}
+			>
+				<MenuItem onClick={() => handleSelectPriority(1)}>
+					<ListItemIcon>
+						<PriorityHighIcon sx={{ color: red[500] }} />
+					</ListItemIcon>
+					<ListItemText primary="High Priority" />
+				</MenuItem>
+				<MenuItem onClick={() => handleSelectPriority(2)}>
+					<ListItemIcon>
+						<LowPriorityIcon sx={{ color: yellow[500] }} />
+					</ListItemIcon>
+					<ListItemText primary="Medium Priority" />
+				</MenuItem>
+				<MenuItem onClick={() => handleSelectPriority(3)}>
+					<ListItemIcon>
+						<AcUnitIcon sx={{ color: blue[500] }} />
+					</ListItemIcon>
+					<ListItemText primary="Low Priority" />
+				</MenuItem>
+				<MenuItem onClick={() => handleSelectPriority(0)}>
+					<ListItemIcon>
+						<QuestionMarkIcon sx={{ color: grey[500] }} />
+					</ListItemIcon>
+					<ListItemText primary="Unset Priority" />
+				</MenuItem>
+			</Menu>
+		</>
 	);
 };
 
