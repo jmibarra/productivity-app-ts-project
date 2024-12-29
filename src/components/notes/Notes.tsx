df7e1665cf58eb2cf087404fcebb58d47fbdf84a
Juan Ibarra <jmibarra86@gmail.com>
2024-12-29 20:01:40 -0300

    se mejoran los estilos de la sección de notas dejandolo mas limpio

--- src/components/notes/Notes.tsx
+++ src/components/notes/Notes.tsx
@@ -3,197 +3,220 @@ import { ReducerActionType } from "../../actions/notes";
 import { notesReducer, initialState } from "../../reducers/notes";
 
 import NoteList from "./NoteList";
-import { ItemHeader, Item, ItemFooter, ItemLoading } from "./styles/NotesStyles";
+import {
+	Container,
+	Header,
+	Footer,
+	ItemLoading,
+	Content,
+} from "./styles/NotesStyles";
 import { properties } from "../../properties";
 import { ListFooterBox } from "../tasks/styles/TasksStyles";
 import { CircularProgress, Pagination } from "@mui/material";
-
+import NotesIcon from "@mui/icons-material/Notes";
 import Cookies from "js-cookie";
 import CreateNoteModalComponent from "./modal/CreateNoteModal";
 import { Note } from "../../interfaces/tasks/interfaces";
 
 interface HeadersInit {
-    headers: Headers;
-    credentials: RequestCredentials;
+	headers: Headers;
+	credentials: RequestCredentials;
 }
 
 const Notes = () => {
-    const [state, dispatch] = useReducer(notesReducer, initialState);
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
-    const [sessionToken, setSessionToken] = useState<string | null>(null);
-    const [loading, setloading] = useState(false)
-    
-    const handlePageChange = (
-        event: React.ChangeEvent<unknown>,
-        value: number
-    ) => {
-        setPage(value);
-    };
+	const [state, dispatch] = useReducer(notesReducer, initialState);
+	const [page, setPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(0);
+	const [sessionToken, setSessionToken] = useState<string | null>(null);
+	const [loading, setloading] = useState(false);
 
-    const fetchAllNotes = useCallback(async (page: number, limit: number) => {
-        
-        try {
-            setloading(true)
-            const headers = new Headers() as HeadersInit["headers"];
-            headers.append("Cookie", `PROD-APP-AUTH=${sessionToken}`);
+	const handlePageChange = (
+		event: React.ChangeEvent<unknown>,
+		value: number
+	) => {
+		setPage(value);
+	};
 
-            const response = await fetch(
-                `${properties.api_url}/notes?page=${page}&limit=${limit}`,
-                {
-                    headers,
-                    credentials: "include",
-                }
-            );
+	const fetchAllNotes = useCallback(
+		async (page: number, limit: number) => {
+			try {
+				setloading(true);
+				const headers = new Headers() as HeadersInit["headers"];
+				headers.append("Cookie", `PROD-APP-AUTH=${sessionToken}`);
 
-            if (response.ok) {
-                const responseJson = await response.json();
-                dispatch({
-                    type: ReducerActionType.GET_ALL_NOTES,
-                    payload: responseJson.notes,
-                });
-                if (responseJson.count > 0) {
-                    setTotalPages(Math.trunc(responseJson.count / limit) + 1);
-                }
-                setloading(false)
-            } else if (response.status === 403) {
-                    throw new Error(
-                        "Forbidden, no hay acceso al recurso solicitado"
-                    ); 
-            } else {
-                throw new Error("Error en la respuesta de la API");
-            }
-            } catch (error) {
-                console.log("Error", error);
-            }
-    },[sessionToken]);
+				const response = await fetch(
+					`${properties.api_url}/notes?page=${page}&limit=${limit}`,
+					{
+						headers,
+						credentials: "include",
+					}
+				);
 
-    const deleteNote = async (id: string) => {
-        try {
-            const headers = new Headers() as HeadersInit["headers"];
-            headers.append("Cookie", `PROD-APP-AUTH=${sessionToken}`);
+				if (response.ok) {
+					const responseJson = await response.json();
+					dispatch({
+						type: ReducerActionType.GET_ALL_NOTES,
+						payload: responseJson.notes,
+					});
+					if (responseJson.count > 0) {
+						setTotalPages(
+							Math.trunc(responseJson.count / limit) + 1
+						);
+					}
+					setloading(false);
+				} else if (response.status === 403) {
+					throw new Error(
+						"Forbidden, no hay acceso al recurso solicitado"
+					);
+				} else {
+					throw new Error("Error en la respuesta de la API");
+				}
+			} catch (error) {
+				console.log("Error", error);
+			}
+		},
+		[sessionToken]
+	);
 
-            const response = await fetch(`${properties.api_url}/notes/${id}`, {
-                method: "DELETE",
-                headers,
-                credentials: "include",
-            });
+	const deleteNote = async (id: string) => {
+		try {
+			const headers = new Headers() as HeadersInit["headers"];
+			headers.append("Cookie", `PROD-APP-AUTH=${sessionToken}`);
 
-            if (!response.ok) {
-                console.log("Error", response);
-            } else {
-                dispatch({ type: ReducerActionType.DELETE_NOTES, payload: id });
-            }
-        } catch (response) {
-            console.log("Error", response);
-        }
-    };
+			const response = await fetch(`${properties.api_url}/notes/${id}`, {
+				method: "DELETE",
+				headers,
+				credentials: "include",
+			});
 
-    const addNote = async (note: Note): Promise<void> => {
-        try {
-            const headers = new Headers();
-            headers.append('Cookie', `PROD-APP-AUTH=${sessionToken}`);
-            headers.append('Content-Type', 'application/json');
+			if (!response.ok) {
+				console.log("Error", response);
+			} else {
+				dispatch({ type: ReducerActionType.DELETE_NOTES, payload: id });
+			}
+		} catch (response) {
+			console.log("Error", response);
+		}
+	};
 
-            const response = await fetch(properties.api_url + "/notes", {
-                method: 'POST',
-                headers,
-                credentials: 'include',
-                body: JSON.stringify(note),
-            });
+	const addNote = async (note: Note): Promise<void> => {
+		try {
+			const headers = new Headers();
+			headers.append("Cookie", `PROD-APP-AUTH=${sessionToken}`);
+			headers.append("Content-Type", "application/json");
 
-            if (!response.ok) {
-                console.log("Error", response);
-                return;
-            }
+			const response = await fetch(properties.api_url + "/notes", {
+				method: "POST",
+				headers,
+				credentials: "include",
+				body: JSON.stringify(note),
+			});
 
-            const responseJson = await response.json();
-            const createdNote: Note = responseJson;
-            
-            dispatch({type: ReducerActionType.ADD_NOTE, payload: createdNote});
-        
-        } catch (error) {
-            console.log("Error", error);
-        }
-    }
+			if (!response.ok) {
+				console.log("Error", response);
+				return;
+			}
 
-    const updateLabels = (id:string, labels: string[]):void => {
-        try{
+			const responseJson = await response.json();
+			const createdNote: Note = responseJson;
 
-            const headers = new Headers() as HeadersInit["headers"];
-            headers.append("Cookie", `PROD-APP-AUTH=${sessionToken}`);
-            headers.append('Content-Type', 'application/json');
+			dispatch({
+				type: ReducerActionType.ADD_NOTE,
+				payload: createdNote,
+			});
+		} catch (error) {
+			console.log("Error", error);
+		}
+	};
 
-            console.log(labels)
-            const data = { labels: labels };
-            fetch(properties.api_url+"/notes/"+id, {
-                method: 'PATCH',
-                headers,
-                credentials: 'include',
-                body: JSON.stringify(data),
-            })
-        }catch(response){
-            console.log("Error", response);
-        }
-    }
+	const updateLabels = (id: string, labels: string[]): void => {
+		try {
+			const headers = new Headers() as HeadersInit["headers"];
+			headers.append("Cookie", `PROD-APP-AUTH=${sessionToken}`);
+			headers.append("Content-Type", "application/json");
 
-    const updateFavorite = (id:string, favorite:boolean):void => {
-        try{
+			console.log(labels);
+			const data = { labels: labels };
+			fetch(properties.api_url + "/notes/" + id, {
+				method: "PATCH",
+				headers,
+				credentials: "include",
+				body: JSON.stringify(data),
+			});
+		} catch (response) {
+			console.log("Error", response);
+		}
+	};
 
-            const headers = new Headers() as HeadersInit["headers"];
-            headers.append("Cookie", `PROD-APP-AUTH=${sessionToken}`);
-            headers.append('Content-Type', 'application/json');
+	const updateFavorite = (id: string, favorite: boolean): void => {
+		try {
+			const headers = new Headers() as HeadersInit["headers"];
+			headers.append("Cookie", `PROD-APP-AUTH=${sessionToken}`);
+			headers.append("Content-Type", "application/json");
 
-            dispatch({type: ReducerActionType.FAVORITE,payload:id})
+			dispatch({ type: ReducerActionType.FAVORITE, payload: id });
 
-            const data = { favorite: !favorite};
-            
-            fetch(properties.api_url+"/notes/"+id, {
-                method: 'PATCH',
-                headers,
-                credentials: 'include',
-                body: JSON.stringify(data),
-            })
-            .then((response) => {
-                if(!response.ok){
-                    dispatch({type: ReducerActionType.FAVORITE,payload:id})
-                    console.log("Error", response);
-                }   
-            })
+			const data = { favorite: !favorite };
 
-        }catch(response){
-            console.log("Error", response);
-        }
-    }
+			fetch(properties.api_url + "/notes/" + id, {
+				method: "PATCH",
+				headers,
+				credentials: "include",
+				body: JSON.stringify(data),
+			}).then((response) => {
+				if (!response.ok) {
+					dispatch({ type: ReducerActionType.FAVORITE, payload: id });
+					console.log("Error", response);
+				}
+			});
+		} catch (response) {
+			console.log("Error", response);
+		}
+	};
 
-    useEffect(() => {
-        const token = Cookies.get('PROD-APP-AUTH');
-        if(token)
-            setSessionToken(token);    
-        
-        fetchAllNotes(page, 9);
-    }, [fetchAllNotes,page]);
+	useEffect(() => {
+		const token = Cookies.get("PROD-APP-AUTH");
+		if (token) setSessionToken(token);
 
-    return (
-        <>   
-            <ItemHeader><h1>Notes</h1><CreateNoteModalComponent addNote={addNote}/></ItemHeader>
-            { loading && 
-                <ItemLoading>
-                    <CircularProgress />
-                </ItemLoading>
-            }
-            { !loading && 
-                <Item>
-                    <NoteList notes={state.notes} deleteNote={deleteNote} updateLabels={updateLabels} updateFavorite={updateFavorite}/>
-                </Item>
-            }
-            <ItemFooter>
-                <ListFooterBox>
-                        <Pagination count={totalPages} page={page} onChange={handlePageChange} variant="outlined" color="primary" />
-                </ListFooterBox>
-            </ItemFooter>
-        </> 
-    )
-}
+		fetchAllNotes(page, 9);
+	}, [fetchAllNotes, page]);
 
-export default Notes
+	return (
+		<Container>
+			<Header>
+				<h1>
+					<NotesIcon /> Notes
+				</h1>
+			</Header>
+			<CreateNoteModalComponent addNote={addNote} />
+			{loading && (
+				<ItemLoading>
+					<CircularProgress />
+				</ItemLoading>
+			)}
+			{!loading && (
+				<Content>
+					<NoteList
+						notes={state.notes}
+						deleteNote={deleteNote}
+						updateLabels={updateLabels}
+						updateFavorite={updateFavorite}
+					/>
+				</Content>
+			)}
+			<Footer>
+				<ListFooterBox>
+					<Pagination
+						count={totalPages}
+						page={page}
+						onChange={handlePageChange}
+						variant="outlined"
+						color="primary"
+					/>
+				</ListFooterBox>
+			</Footer>
+		</Container>
+	);
+};
+
+export default Notes;
--- src/components/notes/styles/NotesStyles.ts
+++ src/components/notes/styles/NotesStyles.ts
@@ -1,7 +1,21 @@
 import { styled } from '@mui/material/styles';
-import { Paper } from "@mui/material";
 
-export const Item = styled(Paper)(({ theme }) => ({
+export const Header = styled("header")({
+	textAlign: "center",
+	backgroundColor: "#f5f5f5",
+	borderRadius: "14px",
+	boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+    marginTop: "50px",
+});
+
+export const Container = styled("div")({
+	display: "flex",
+	flexDirection: "column",
+	padding: "16px",
+	gap: "16px",
+});
+
+export const ItemLoading = styled("main")(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
@@ -10,29 +24,15 @@ export const Item = styled(Paper)(({ theme }) => ({
     marginTop: '10px'
 }));
 
-export const ItemHeader = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    marginTop: '50px'
-}));
+export const Content = styled("main")({
+	display: "flex",
+	flexDirection: "column",
+	gap: "16px",
+});
 
-export const ItemLoading = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    marginTop: '10px'
-}));
-
-export const ItemFooter = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    marginTop: '10px'
-}));
+export const Footer = styled("footer")({
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	padding: "8px 0",
+});
