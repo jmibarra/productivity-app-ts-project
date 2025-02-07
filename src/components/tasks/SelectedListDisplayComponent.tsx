787c02b3c84730f0355555d392e65a77863f85b7
Juan Ibarra <jmibarra86@gmail.com>
2025-02-07 19:53:23 -0300

    Logro mostrar las listas en la vista de tareas obteniendo los datos de la api

--- src/components/tasks/SelectedListDisplayComponent.tsx
+++ src/components/tasks/SelectedListDisplayComponent.tsx
@@ -1,6 +1,11 @@
 import { Box, ListItemText } from "@mui/material";
-import React from "react";
+import { useCallback, useEffect, useReducer, useState } from "react";
+import Cookies from "js-cookie";
 import ListCustomIconComponent from "../common/Drawer/Sections/TaskLists/ListCustomIconComponent";
+import { fetchUserLists } from "../../services/taskListsServices";
+import { ReducerTaskListActionType } from "../../actions/tasksLists";
+import { initialState, taskListsReducer } from "../../reducers/taksLists";
+import { TaskList } from "../../interfaces";
 
 interface SelectedListDisplayComponentProps {
 	selectedListId: string;
@@ -8,12 +13,51 @@ interface SelectedListDisplayComponentProps {
 const SelectedListDisplayComponent = ({
 	selectedListId,
 }: SelectedListDisplayComponentProps) => {
+	const [selectdList, setSelectedList] = useState<TaskList | undefined>(
+		undefined
+	);
+	const [sessionToken, setSessionToken] = useState<string | null>(null);
+	const [state, dispatch] = useReducer(taskListsReducer, initialState);
+
+	const fetchAllLists = useCallback(async () => {
+		try {
+			const responseJson = await fetchUserLists(sessionToken);
+			dispatch({
+				type: ReducerTaskListActionType.GET_USER_LISTS,
+				payload: responseJson,
+			});
+		} catch (error) {
+			console.error("Error fetching lists", error);
+		}
+	}, [sessionToken]);
+
+	useEffect(() => {
+		const token = Cookies.get("PROD-APP-AUTH");
+		if (token) {
+			setSessionToken(token);
+			fetchAllLists();
+		}
+		setSelectedList(
+			state.taskLists.find((list) => list._id === selectedListId)
+		);
+	}, [fetchAllLists]);
+
+	useEffect(() => {
+		setSelectedList(
+			state.taskLists.find((list) => list._id === selectedListId) || {
+				_id: "0",
+				name: "Todas",
+				icon: "All",
+			}
+		);
+	}, [state.taskLists, selectedListId]);
+
 	return (
 		<Box display="flex" justifyContent="space-between" width="100%">
 			<Box display="flex" alignItems="center" justifyContent={"center"}>
-				<ListCustomIconComponent listIcon="Inbox" />
+				<ListCustomIconComponent listIcon={selectdList?.icon} />
 				<ListItemText
-					primary={selectedListId}
+					primary={selectdList?.name}
 					primaryTypographyProps={{
 						color: "primary",
 						fontWeight: "medium",
