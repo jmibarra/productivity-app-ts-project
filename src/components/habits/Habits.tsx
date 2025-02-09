e037b0682b11bd4076d3723bbdbf43aa389f1b62
Juan Ibarra <jmibarra86@gmail.com>
2025-02-09 20:55:00 -0300

    Comienzo a armar el modulo de habitos con datos de la base de datos

--- src/components/habits/Habits.tsx
+++ src/components/habits/Habits.tsx
@@ -9,91 +9,6 @@ import { ReducerActionType as HabitsReducerActionType } from "../../actions/habi
 import { habitsReducer, initialState } from "../../reducers/habits";
 import { fetchHabits } from "../../services/habitsServices";
 
-/*
-Interfaz de los habitos
-
-export interface Habit {
-    _id?: string;
-    user_id: string; // ID del usuario
-    name: string; // Nombre del hábito
-    description?: string; // Descripción opcional
-    icon?: string; // Identificador del icono
-    goal: {
-      type: "daily" | "quantity"; // Tipo de objetivo
-      target: number; // Objetivo (e.g., 1 para cumplido o cantidad específica)
-    };
-    frequency: {
-      type: "daily" | "weekly" | "interval"; // Tipo de frecuencia
-      details: {
-        days_of_week?: string[]; // Días específicos para 'daily'
-        weekly_target?: number; // Número de días por semana para 'weekly'
-        interval_days?: number; // Días entre repeticiones para 'interval'
-      };
-    };
-    streak: number; // Días consecutivos cumplidos
-    created_at?: Date;
-    updated_at?: Date;
-}
-
-/*/
-
-const mockHabits: Habit[] = [
-	{
-		_id: "1",
-		user_id: "123",
-		name: "Journaling / Objetivos del día",
-		icon: "📔",
-		goal: {
-			type: "daily",
-			target: 1,
-		},
-		frequency: {
-			type: "daily",
-			details: {
-				days_of_week: [
-					"monday",
-					"tuesday",
-					"wednesday",
-					"thursday",
-					"friday",
-					"saturday",
-					"sunday",
-				],
-			},
-		},
-		streak: 250,
-		created_at: new Date(),
-		updated_at: new Date(),
-	},
-	{
-		_id: "2",
-		user_id: "123",
-		name: "Meditación",
-		icon: "🧘",
-		goal: {
-			type: "daily",
-			target: 1,
-		},
-		frequency: {
-			type: "daily",
-			details: {
-				days_of_week: [
-					"monday",
-					"tuesday",
-					"wednesday",
-					"thursday",
-					"friday",
-					"saturday",
-					"sunday",
-				],
-			},
-		},
-		streak: 250,
-		created_at: new Date(),
-		updated_at: new Date(),
-	},
-];
-
 const Habits = () => {
 	const [state, dispatch] = useReducer(habitsReducer, initialState);
 	const [sessionToken, setSessionToken] = useState<string | null>(null);
@@ -137,7 +52,7 @@ const Habits = () => {
 				<Content>
 					{/* Columna izquierda: Lista de hábitos */}
 					<HabitsList
-						habits={mockHabits}
+						habits={state.habits}
 						setSelectedHabit={setSelectedHabit}
 					/>
 					{/* Columna derecha: Estadísticas y calendario mensual */}
